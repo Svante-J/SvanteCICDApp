25e39ad956125e9aa1d4904ce8a02f564bc1cbf9
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-11-09 14:51:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SvanteSarahsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
