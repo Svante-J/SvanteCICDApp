7ec3b0ed59019a1570c511ff87b2ae92d4d8b8fc
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-11-09 15:02:11 +0100

    init

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SvanteSarahsApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("dir");
         }
     }
 }
