76bbc99185184cf7bdeba969f0610ed5b8b9a71f
Svante-J <72524651+Svante-J@users.noreply.github.com>
2022-01-03 22:02:08 +0100

    Intergration

---
+++ SvantesTester/IntergrationTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using SvanteSarahsApp.Controller;
+using SvanteSarahsApp.Utility;
+using SvantesTester;
+
+namespace SvantesTester
+{
+    [TestFixture]
+    public class IntergrationTest
+    {
+        [Test]
+        public void IntergrationTest_LogginIfAdmin()
+        {
+            // Arrange
+            var mocklist = MockData.FillMockList();
+            const string name = "admin1";
+            const string password = "admin1234";
+            // Act
+            var logginSucces = IAccount.FindUser(name, password, mocklist);
+       
+            
+            
+
+            // Assert
+            Assert.IsTrue(logginSucces);
+            Assert.IsNotNull(LoggedInUser.GetActiveUser());
+            Assert.IsTrue(LoggedInUser.GetActiveUser().IsAdmin);
+           
+
+        }
+
+       
+    }
+}
